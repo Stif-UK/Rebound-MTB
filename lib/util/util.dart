092fdbee1150bd5d97fab042dc9e4a65e2468eb9
stifdev <stifdev.uk@gmail.com>
2020-04-14 17:22:46 +0100

    utility

---
+++ lib/util/util.dart
@@ -0,0 +1,10 @@
+/*
+Openweathermap.com API key - note, this file should be included in gitIgnore
+ */
+
+final appId = "expired_token";
+/*
+Default latitude and longitude for Lennox Forest
+ */
+final String defaultLat = "55.968999";
+final String defaultLon = "-4.226788";
